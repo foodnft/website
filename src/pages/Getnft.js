0e197777952d63c7477d19f97b853721a9711132
Vipul Tanwar <thevipultanwar@gmail.com>
2023-02-14 23:15:24 +0530

    Add validation in mobile no and otp

--- src/pages/Getnft.js
+++ src/pages/Getnft.js
@@ -15,6 +15,17 @@ const Getnft = () => {
         setShowPopUp(!showPopUp);
     };
 
+
+    const [value, setValue] = useState('');
+
+    const handleKeyPress = (event) => {
+        const keyCode = event.keyCode || event.which;
+        const keyValue = String.fromCharCode(keyCode);
+        if (!/^\d$/.test(keyValue)) {
+            event.preventDefault();
+        }
+    };
+
     return (
         <div className=" min-w- min-h-[100vh] overflow-x-hidden  ">
             <div className='relative ' >
@@ -30,12 +41,18 @@ const Getnft = () => {
                     <h1 className='text-[2.4rem] text-center font-bold my-6' >Get Nft</h1>
                     {/* <button className=' bg-[#c32f27] text-white py-4 w-[92%] ml-[4%] mx-auto my-  text-[1.4rem]  font-bold  '>Click here to start</button> */}
                     <div className=' h-[50vh] w-[92%] mx-auto bg-black mt-20 my-2 rounded-xl relative  flex flex-col justify-center ' >
-                        {showPopUp ?  <></> : <div className='flex flex-col' >
-                            <h2 className=' text-[#fdf523] text-2xl  my-10 text-center mx-2 text' >Collect the Tastiest Chicken NFTs</h2>
-                            <div >
-                                <form className=' flex justify-center' >
+                        {showPopUp ? <></> : <div className='flex flex-col' >
 
-                                    <input type={Number} className="h-14 w-[70%] font-bold text-2xl " ></input>
+
+
+                            <h2 className=' text-[#fdf523] text-2xl  my-10 text-center mx-2 text' >Collect the Tastiest Chicken NFTs</h2>
+
+                            <div >
+                                <form className=' flex justify-center gap-4' >
+                                    <select id="cars" name="cars" className='h-14 w-[20%] text-center font-bold text-2xl '>
+                                        <option value="mal" className='font-bold text-2xl  ' >+60</option>
+                                    </select>
+                                    <input type={Number} onKeyPress={handleKeyPress} maxLength={15} className="h-14 w-[60%] font-bold text-2xl text-center " ></input>
 
                                 </form>
                             </div>
@@ -46,26 +63,26 @@ const Getnft = () => {
 
                             <button className='bg-[#db7c26] my-10 py-2 inline w-[150px] rounded-3xl mx-auto  font-bold text-lg' onClick={handleClick}>Send Otp </button>
 
-                        </div> }
+                        </div>}
                         {showPopUp && (
-                            <div style={{padding: '20px' }} className="relative top-0 h-[100vh] flex flex-col bg-black  rounded-xl " >
+                            <div style={{ padding: '20px' }} className="relative top-0 h-[100vh] flex flex-col bg-black  rounded-xl " >
                                 <h2 className=' text-[#fdf523] text-2xl  my-10 text-center mx-2 text' >Collect the Tastiest Chicken NFTs</h2>
                                 <div >
                                     <form className=' flex flex-col justify-center text-white ' >
-                                        <label className='text-center text-2xl ' >Input Otp</label>
+                                        <label className='text-center text-2xl mb-5 ' >Input Otp</label>
                                         <div className='flex gap-2 justify-center'>
-                                        <input type={Number} className="h-14 w-[20%] font-bold text-2xl " ></input>
-                                        <input type={Number} className="h-14 w-[20%] font-bold text-2xl " ></input>
-                                        <input type={Number} className="h-14 w-[20%] font-bold text-2xl " ></input>
-                                        <input type={Number} className="h-14 w-[20%] font-bold text-2xl " ></input>
+                                            <input type={Number} onKeyPress={handleKeyPress} maxLength={1} className="h-14 w-[20%] font-bold text-black text-2xl text-center  " ></input>
+                                            <input type={Number} onKeyPress={handleKeyPress} maxLength={1} className="h-14 w-[20%] font-bold text-black text-2xl text-center  " ></input>
+                                            <input type={Number} onKeyPress={handleKeyPress} maxLength={1} className="h-14 w-[20%] font-bold text-black text-2xl text-center  " ></input>
+                                            <input type={Number} onKeyPress={handleKeyPress} maxLength={1} className="h-14 w-[20%] font-bold text-black text-2xl text-center  " ></input>
                                         </div>
-
+                        
                                     </form>
                                 </div>
-                                    <Link to="/mycollection" className=' mx-auto' >
-                            <button className='bg-[#db7c26] my-10 py-2 inline w-[150px] rounded-3xl mx-auto  font-bold text-lg' >Confirm</button>
-                            
-                            </Link>
+                                <Link to="/mycollection" className=' mx-auto' >
+                                    <button className='bg-[#db7c26] my-10 py-2 inline w-[150px] rounded-3xl mx-auto  font-bold text-lg' >Confirm</button>
+
+                                </Link>
                                 <button onClick={handleClick}>Close</button>
                             </div>
                         )}
