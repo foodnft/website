f0f89d5a82c857a28bf7cd00ab00a2c93b0c3af3
Vipul Tanwar <thevipultanwar@gmail.com>
2023-02-17 12:30:56 +0530

    My collection page changes

--- src/pages/Getnft.js
+++ src/pages/Getnft.js
@@ -33,7 +33,7 @@ const Getnft = () => {
               <Header/>
                 <div className='relative z-0 bg-[#F9DC5C] p-2 h-[920px]'>
                     <img src={bglayer} className='w-[100%] absolute top-0 bottom-0 z-[-1] '></img>
-                    <h1 className='text-[2.4rem] text-center font-bold my-6'>Get Nft</h1>
+                    <h1 className='text-[2.4rem] text-center font-bold my-6'>Get NFT</h1>
                     {/* <button className='bg-[#c32f27] text-white py-4 w-[92%] ml-[4%] mx-auto my-  text-[1.4rem]  font-bold  '>Click here to start</button> */}
                     <div className='h-[50vh] w-[92%] mx-auto bg-black mt-20 my-2 rounded-xl relative  flex flex-col justify-center '>
                      <div className='flex flex-col'>
--- src/pages/Mycollection.js
+++ src/pages/Mycollection.js
@@ -15,15 +15,17 @@ const Mycollection = () => {
                 <Header/>
                 <div className='relative z-0 bg-[#F9DC5C] p-2 h-[920px]'>
                     <img src={bglayer} className='w-[100%] absolute top-0 bottom-0 z-[-1] '></img>
-                    <h1 className='text-[2.4rem] text-center font-bold my-6'>Get Nft</h1>
+                    <h1 className='text-[2.4rem] text-center font-bold my-6'>Mint NFT</h1>
                     <Link to='/yournft'className='mx-auto'>
 
-                    <button className='bg-[#780116] text-white py-4 w-[92%] ml-[4%] mx-auto my-  text-[1.4rem]  font-bold  '>Generate NFT</button>
+                    <button className='bg-[#780116] text-white py-4 w-[92%] ml-[4%] mx-auto text-[1.4rem] font-bold drop-shadow-xl
+
+'>Generate NFT</button>
                     </Link>
 
                     <div className='h-[50vh] w-[92%] mx-auto bg-black mt-20 my-2 rounded-xl  flex flex-col justify-center '>
                         <img src={Success} className='w-[80%] mx-auto'></img>
-                            <button className='bg-[#db7c26] my-10 py-3  inline w-[150px] rounded-3xl mx-auto  font-bold text-lg'>Start Buying</button>
+                            <button className='bg-[#db7c26] my-10 py-3  inline w-[180px] rounded-3xl mx-auto  font-bold text-lg'>Your Collection</button>
                             
                         <p>Terms and Conditions | Private Policy</p>
                     </div>
