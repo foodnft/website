266386810d6d17db320f2670abefd6b5a61b8042
Vipul Tanwar <thevipultanwar@gmail.com>
2023-02-17 11:57:04 +0530

    Addd link to my collection

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+
 import chicken from '../images/chicken.svg'
 import mail from '../images/mail.svg'
 import telegram from '../images/telegram.svg'
@@ -6,9 +8,13 @@ import telegram from '../images/telegram.svg'
 const Header = () => {
     return (
         <header className='bg-black h-[60px] flex justify-between  px-3 py-2 '>
-            <img src={chicken} ></img>
+            <Link to="/" >
+                <img src={chicken} ></img>
+            </Link>
             <div className='my-auto'>
-                <button className='bg-[#C32F27] text-white px-4 py-2 rounded-[40px] text-[1.0rem] mx-1'>My Collection</button>
+                <Link to='/yournft' className='mx-auto'>
+                    <button className='bg-[#C32F27] text-white px-4 py-2 rounded-[40px] text-[1.0rem] mx-1'>My Collection</button>
+                </Link>
             </div>
         </header>
     )
