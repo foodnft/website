27afd823b7bb78a13c43b7f2faaf0924c39e358d
Vipul Tanwar <thevipultanwar@gmail.com>
2023-02-17 13:05:02 +0530

    Add pop up in my collection

--- src/pages/Mycollection.js
+++ src/pages/Mycollection.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import chicken from '../images/chicken.svg'
 import mail from '../images/mail.svg'
 import telegram from '../images/telegram.svg'
@@ -9,35 +9,36 @@ import Header from '../components/Header'
 import Footer from '../components/Footer'
 
 const Mycollection = () => {
+
+    const [nft, setNft] = useState(false)
+
+    function generateNft() {
+        setNft(true)
+    }
+
     return (
         <div className='min-w-max min-h-[100vh] overflow-x-hidden  '>
             <div className='relative '>
-                <Header/>
+                <Header />
                 <div className='relative z-0 bg-[#F9DC5C] p-2 h-[920px]'>
                     <img src={bglayer} className='w-[100%] absolute top-0 bottom-0 z-[-1] '></img>
                     <h1 className='text-[2.4rem] text-center font-bold my-6'>Mint NFT</h1>
-                    <Link to='/yournft'className='mx-auto'>
+                    <button onClick={generateNft} className='bg-[#780116] text-white py-4 w-[92%] ml-[4%] mx-auto text-[1.4rem] font-bold drop-shadow-xl'>Generate NFT</button>
+                    {
+                        nft ? <div className='h-[50vh] w-[92%] mx-auto bg-black mt-20 my-2 rounded-xl  flex flex-col justify-center drop-shadow-xl'>
+                            <img src={Success} className='w-[80%] mx-auto'></img>
+                            <Link to="/yournft" className='mx-auto' >
+                                <button className='bg-[#db7c26] my-10 py-3  inline w-[180px] rounded-3xl mx-auto  font-bold text-lg'>Your Collection</button>
 
-                    <button className='bg-[#780116] text-white py-4 w-[92%] ml-[4%] mx-auto text-[1.4rem] font-bold drop-shadow-xl
+                            </Link>
 
-'>Generate NFT</button>
-                    </Link>
-
-                    <div className='h-[50vh] w-[92%] mx-auto bg-black mt-20 my-2 rounded-xl  flex flex-col justify-center drop-shadow-xl
-
-'>
-                        <img src={Success} className='w-[80%] mx-auto'></img>
-                        <Link to="/yournft" className='mx-auto' >
-                            <button className='bg-[#db7c26] my-10 py-3  inline w-[180px] rounded-3xl mx-auto  font-bold text-lg'>Your Collection</button>
-
-                        </Link>
-                            
-                        <p>Terms and Conditions | Private Policy</p>
-                    </div>
+                            <p>Terms and Conditions | Private Policy</p>
+                        </div> : <></>
+                    }
 
                 </div>
 
-                <Footer/>
+                <Footer />
             </div>
         </div>
 
