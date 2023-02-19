e42dc4b3f914df66f3ce65c546c70de9c4b0e623
Vipul Tanwar <thevipultanwar@gmail.com>
2023-02-19 16:55:17 +0530

    Restore old

--- src/App.js
+++ src/App.js
@@ -1,30 +1,24 @@
-import { Suspense, lazy } from "react";
-import { Route, Routes, useLocation } from "react-router-dom";
 
-import HeaderComponent from "./components/Header";
-import FooterComponent from "./components/Footer";
-const Home = lazy(() => import("./pages/Home"));
-const Getnft = lazy(() => import("./pages/GetNFT/GetNFT"));
-const Mycollection = lazy(() => import("./pages/Mycollection"));
-const Yournft = lazy(() => import("./pages/Yournft"));
-const Enterotp = lazy(() => import("./pages/Enterotp"));
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from './pages/Home';
+import Getnft from "./pages/Getnft";
+import Mycollection from "./pages/Mycollection";
+import Yournft from "./pages/Yournft";
+import './App.css';
+import Enterotp from "./pages/Enterotp";
 
 function App() {
-  const { pathname } = useLocation();
   return (
-    <>
-      <HeaderComponent />
-      <Suspense fallback={<div>Loading..</div>}>
-        <Routes>
-          <Route path="/*" element={<Home />} />
-          <Route path="/getnft" element={<Getnft />} />
-          <Route path="/enterotp" element={<Enterotp />} />
-          <Route path="/mycollection" element={<Mycollection />} />
-          <Route path="/yournft" element={<Yournft />} />
-        </Routes>
-      </Suspense>
-      {pathname === "/" && <FooterComponent />}
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/*" element={<Home />} />
+        <Route path="/getnft" element={<Getnft/>} />
+        <Route path="/enterotp" element={<Enterotp/>} />
+        <Route path="/mycollection" element={<Mycollection/>} />
+        <Route path="/yournft" element={<Yournft/>} />
+
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- src/pages/Mycollection.js
+++ src/pages/Mycollection.js
@@ -1,41 +1,48 @@
-import React from "react";
-import { Link } from "react-router-dom";
-
-import chicken from "../images/chicken.svg";
-import mail from "../images/mail.svg";
-import telegram from "../images/telegram.svg";
-import bglayer from "../images/bglayer.svg";
-import Success from "../images/Success.svg";
+import React, { useState } from 'react'
+import chicken from '../images/chicken.svg'
+import mail from '../images/mail.svg'
+import telegram from '../images/telegram.svg'
+import bglayer from '../images/bglayer.svg'
+import Success from '../images/Success.svg'
+import { Link } from 'react-router-dom'
+import Header from '../components/Header'
+import Footer from '../components/Footer'
 
 const Mycollection = () => {
-  return (
-    <div className="min-w-max min-h-[100vh]  overflow-hidden">
-      <div className="relative ">
-        <div className="relative z-0 bg-[#F9DC5C] p-2 h-[920px]">
-          <img
-            src={bglayer}
-            className="w-[100%] absolute top-0 bottom-0 z-[-1] "
-          ></img>
-          <h1 className="text-[2.4rem] text-center font-bold my-6">Get Nft</h1>
-          <Link to="/yournft" className="mx-auto">
-            <button className="bg-[#780116] text-white py-4 w-[92%] ml-[4%] mx-auto my-  text-[1.4rem]  font-bold  ">
-              Generate NFT
-            </button>
-          </Link>
 
-          <div className="h-[50vh] w-[92%] mx-auto bg-black mt-20 my-2 rounded-xl  flex flex-col justify-center ">
-            <img src={Success} className="w-[80%] mx-auto"></img>
-            <button className="bg-[#db7c26] my-10 py-3  inline w-[150px] rounded-3xl mx-auto  font-bold text-lg">
-              Start Buying
-            </button>
+    const [nft, setNft] = useState(false)
 
-            <p>Terms and Conditions | Private Policy</p>
-          </div> 
+    function generateNft() {
+        setNft(true)
+    }
+
+    return (
+        <div className='min-w-max min-h-[100vh] overflow-x-hidden  '>
+            <div className='relative '>
+                <Header />
+                <div className='relative z-0 bg-[#F9DC5C] p-2 h-[920px]'>
+                    <img src={bglayer} className='w-[100%] absolute top-0 bottom-0 z-[-1] '></img>
+                    <h1 className='text-[2.4rem] text-center font-bold my-6'>Mint NFT</h1>
+                    <button onClick={generateNft} className='bg-[#780116] text-white py-4 w-[92%] ml-[4%] mx-auto text-[1.4rem] font-bold drop-shadow-xl'>Generate NFT</button>
+                    {
+                        nft ? <div className='h-[50vh] w-[92%] mx-auto bg-black mt-20 my-2 rounded-xl  flex flex-col justify-center drop-shadow-xl'>
+                            <img src={Success} className='w-[80%] mx-auto'></img>
+                            <Link to="/yournft" className='mx-auto' >
+                                <button className='bg-[#db7c26] my-10 py-3  inline w-[180px] rounded-3xl mx-auto  font-bold text-lg'>Your Collection</button>
+
+                            </Link>
+
+                            <p>Terms and Conditions | Private Policy</p>
+                        </div> : <></>
+                    }
+
+                </div>
+
+                <Footer />
+            </div>
         </div>
-      </div>
-    </div>
-  );
-  
-};
 
-export default Mycollection;
+    )
+}
+
+export default Mycollection
