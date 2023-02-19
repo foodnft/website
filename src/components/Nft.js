706d3107436183a3133f91fceb56ea682e549147
Vipul Tanwar <thevipultanwar@gmail.com>
2023-02-19 18:34:45 +0530

    Nft changes

--- src/components/Nft.js
+++ src/components/Nft.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom'
 
 const Nft = () => {
     return (
-        <div className='h-[50vh] w-[92%] mx-auto bg-black mt-20 my-2 rounded-xl flex flex-col drop-shadow-xl ' >
+        <div className=' h-[400px] max-w-[400px] mx-auto bg-black mt-20 my-2 rounded-xl flex flex-col drop-shadow-xl ' >
             <div className='w-[90%] bg-slate-400 h-[80%] mx-[5%] my-5 rounded-lg ' ></div>
             <Link to='/' className='mx-auto'>
                 <div className='flex text-white max-w-[90%] mx-[6%] '>
